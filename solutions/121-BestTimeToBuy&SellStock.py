7cb9650b29b129ba9c764a89fa80efd4cd047e8d
Reyansh Patange <reyansh.patange@gmail.com>
2022-02-28 22:22:47 -0500

    solved #121 - Best Time to Buy & Sell Stock

---
+++ solutions/121-BestTimeToBuy&SellStock.py
@@ -0,0 +1,17 @@
+# Notes: example of Sliding Window algorithm - keeps track of left and right pointers. iterate through prices array, keep track of left pointer called currMin. if currProfit > maxProfit, set maxProfit = currProfit. if the right pointer (prices[i]) < left pointer (currMin), set currMin to the right pointer and keep going. at the end, return maxProfit
+# Space Complexity: O(n)
+# Time  Complexity: O(n)
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if len(prices) == 1:
+            return 0
+        currMin = prices[0]
+        maxProfit = 0
+        for i in range(1, len(prices)):
+            currProfit = (prices[i] - currMin)
+            if (currProfit > maxProfit):
+                maxProfit = currProfit
+            if (prices[i] < currMin):
+                currMin = prices[i]
+        return maxProfit
