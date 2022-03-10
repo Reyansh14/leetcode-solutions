af44c6a955e2904b6e274fe9ace79ee3a86dc6c4
Reyansh Patange <reyansh.patange@gmail.com>
2022-03-10 07:59:51 -0500

    fixed error in iterative DFS solution

--- solutions/104-MaximumDepthOfBinaryTree.py
+++ solutions/104-MaximumDepthOfBinaryTree.py
@@ -28,7 +28,7 @@ class Solution:
         while stack:
             node, depth = stack.pop()
             if node:
-                result = max(res, depth)
+                result = max(result, depth)
                 stack.append([node.left, depth + 1])
                 stack.append([node.right, depth + 1])
         return result
