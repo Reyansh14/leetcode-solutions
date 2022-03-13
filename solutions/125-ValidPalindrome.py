2a643d0e0f75015e0f771fbc37e473b66075c7aa
Reyansh Patange <reyansh.patange@gmail.com>
2022-03-13 17:56:56 -0400

    added more space efficient solution for #125

--- solutions/125-ValidPalindrome.py
+++ solutions/125-ValidPalindrome.py
@@ -1,7 +1,6 @@
 # Notes: Use 2 pointers and scan the string after converting it to only have alphanumeric characters in it
 # Space Complexity: O(n)
 # Time  Complexity: O(n)
-
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         lowercaseStr = s.lower()
@@ -18,4 +17,28 @@ class Solution:
                 return False
             first += 1
             last -= 1
+        return True
+
+
+# Notes: More space efficient solution -> Use 2 pointers again, use ord to see if it's alphanumeric; while left/right pointer isn't alphanumeric, keep incrementing/decrementing as required
+# Space Complexity: O(1)
+# Time  Complexity: O(n)
+class Solution:
+    def alphaNumeric(self, char):
+        return (ord('A') <= ord(char) <= ord('Z') or
+                ord('a') <= ord(char) <= ord('z') or
+                ord('0') <= ord(char) <= ord('9'))
+
+    def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
+
+        while l < r:
+            while l < r and not self.alphaNumeric(s[l]):
+                l += 1
+            while r > l and not self.alphaNumeric(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l += 1
+            r -= 1
         return True
