6e8755009642f1301b242643d06c756761442465
Reyansh Patange <reyansh.patange@gmail.com>
2022-03-13 23:43:03 -0400

    Solved #74-Search 2D Matrix

--- solutions/74-Search2DMatrix.py
+++ solutions/74-Search2DMatrix.py
@@ -12,3 +12,40 @@ class Solution:
                     targetArr = subArr.copy()
 
         return True if target in targetArr else False
+
+
+# Notes: Use binary search to find the correct row, and binary search on that correct row to see if the target exists within that subarray.
+# Space Complexity: O(log n)
+# Time  Complexity: O(log n)
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        rows = len(matrix)
+        cols = len(matrix[0])
+        top = 0
+        bottom = rows - 1
+
+        while top <= bottom:
+            middleRow = (top + bottom) // 2
+            print(middleRow, top, bottom)
+            if target > matrix[middleRow][-1]:
+                top = middleRow + 1
+            elif target < matrix[middleRow][0]:
+                bottom = middleRow - 1
+            else:
+                break
+
+        if not (top <= bottom):
+            return False
+
+        row = (top + bottom) // 2
+        l = 0
+        r = cols - 1
+        while l <= r:
+            m = (l + r) // 2
+            if target > matrix[row][m]:
+                l = m + 1
+            elif target < matrix[row][m]:
+                r = m - 1
+            else:
+                return True
+        return False
