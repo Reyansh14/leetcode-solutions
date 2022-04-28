2fa3a52ce129a15e379574c0d38c5a56da71e9cc
Reyansh Patange <reyansh.patange@gmail.com>
2022-04-28 19:46:28 -0400

    solved #79 - Word Search

---
+++ solutions/79-WordSearch.py
@@ -0,0 +1,34 @@
+# Notes: Keep track of visited tiles in the path set. In the dfs function, return true if charIdx reaches len of word; return false if out of bounds, or if char doesn't match current tile, or if tile is in path.
+# Time  Complexity: O(n * m * 4^(word.length))
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        rows = len(board)
+        cols = len(board[0])
+        path = set()
+
+        def dfs(row, col, charIdx):
+            if charIdx == len(word):
+                return True
+
+            if (row < 0 or col < 0
+                or row >= rows or col >= cols
+                or word[charIdx] != board[row][col]
+                    or (row, col) in path):
+                return False
+
+            path.add((row, col))
+
+            result = (dfs(row + 1, col, charIdx + 1) or
+                      dfs(row - 1, col, charIdx + 1) or
+                      dfs(row, col + 1, charIdx + 1) or
+                      dfs(row, col - 1, charIdx + 1))
+
+            path.remove((row, col))
+
+            return result
+
+        for row in range(rows):
+            for col in range(cols):
+                if dfs(row, col, 0):
+                    return True
+        return False
