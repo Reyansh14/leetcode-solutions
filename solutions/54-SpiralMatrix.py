986a186e80438f8d9468c11659825e6867d4747e
Reyansh Patange <35512531+Reyansh14@users.noreply.github.com>
2022-11-14 12:49:37 -0500

    solved #54-SpiralMatrix

---
+++ solutions/54-SpiralMatrix.py
@@ -0,0 +1,31 @@
+# Notes: Printed out the elements of a matrix in spiral order by keeping track of a top, bottom, left, and right pointer and adjusting them as each row/column is read.
+# Time  Complexity: O(n * m)
+# Space Complexity: O(1) (not counting output array)
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        spiral = []
+        l, r = 0, len(matrix[0])
+        up, down = 0, len(matrix)
+
+        while l < r and up < down:
+            for i in range(l, r):
+                spiral.append(matrix[up][i])
+            up += 1
+
+            for i in range(up, down):
+                spiral.append(matrix[i][r - 1])
+            r -= 1
+
+            if not (l < r and up < down):
+                break
+
+            for i in range(r - 1, l - 1, -1):
+                spiral.append(matrix[down - 1][i])
+            down -= 1
+
+            for i in range(down - 1, up - 1, -1):
+                spiral.append(matrix[i][l])
+            l += 1
+
+        return spiral
+            
