956caf78b737ba2850367f68930a43a3b32070e6
Reyansh Patange <35512531+Reyansh14@users.noreply.github.com>
2023-09-09 15:12:00 -0400

    solved search 2D matrix

--- solutions/74-Search2DMatrix.py
+++ solutions/74-Search2DMatrix.py
@@ -1,57 +1,35 @@
 # Notes: 
-# Iterate through outer array to find an array that potentially has the target; 
-# if the target is in this targetArray, return True, else return False
+# Use binary search on matrix array to first find the targetRow; if it doesn't give us a targetRow return False.
+# Then, if targetRow was found, run binary search again but on targetRow this time to find target and return True if found.
+# Else, return False
 
-# Space Complexity: O(n)
-# Time  Complexity: O(n)
+# Space Complexity: O(1)
+# Time  Complexity: O(log(m * n))
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        targetArr = []
-        for subArr in matrix:
-            if (subArr[0] <= target) and (target <= subArr[-1]):
-                if (subArr[0] == target) or (target == subArr[-1]):
-                    return True
-                else:
-                    targetArr = subArr.copy()
-
-        return True if target in targetArr else False
-
-
-# Notes: 
-# Use binary search to find the correct row, and binary search on that correct row,
-# to see if the target exists within that subarray.
-
-# Space Complexity: O(log n)
-# Time  Complexity: O(log n)
-class Solution:
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        rows = len(matrix)
-        cols = len(matrix[0])
-        top = 0
-        bottom = rows - 1
-
-        while top <= bottom:
-            middleRow = (top + bottom) // 2
-            print(middleRow, top, bottom)
-            if target > matrix[middleRow][-1]:
-                top = middleRow + 1
-            elif target < matrix[middleRow][0]:
-                bottom = middleRow - 1
+        rowTop, rowBot = 0, len(matrix) - 1
+        targetRow = -1
+        while rowTop <= rowBot:
+            rowMid = (rowTop + rowBot) // 2
+            if matrix[rowMid][0] > target:
+                rowBot = rowMid - 1
+            elif matrix[rowMid][-1] < target:
+                rowTop = rowMid + 1
             else:
+                targetRow = rowMid
                 break
-
-        if not (top <= bottom):
+        if targetRow == -1:
             return False
 
-        row = (top + bottom) // 2
-        l = 0
-        r = cols - 1
-        while l <= r:
-            m = (l + r) // 2
-            if target > matrix[row][m]:
-                l = m + 1
-            elif target < matrix[row][m]:
-                r = m - 1
+        targetArr = matrix[targetRow]
+        low, high = 0, len(targetArr) - 1
+        while low <= high:
+            mid = (low + high) // 2
+            if targetArr[mid] < target:
+                low = mid + 1
+            elif targetArr[mid] > target:
+                high = mid - 1
             else:
                 return True
         return False
+
