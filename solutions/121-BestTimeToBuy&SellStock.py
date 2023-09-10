b95c2f5a77db805b704e1c486726cb18e09f423f
Reyansh Patange <35512531+Reyansh14@users.noreply.github.com>
2023-09-10 13:45:24 -0400

    solved best tine to buy&sell stock

--- solutions/121-BestTimeToBuy&SellStock.py
+++ solutions/121-BestTimeToBuy&SellStock.py
@@ -1,23 +1,26 @@
 # Notes: 
 # example of Sliding Window algorithm - keeps track of left and right pointers. 
-# iterate through prices array, keep track of left pointer called currMin. 
-# if currProfit > maxProfit, set maxProfit = currProfit. 
-# if the right pointer (prices[i]) < left pointer (currMin), set currMin to the right pointer and keep going. 
+# initialize l, r pointers at first 2 positions of array.
+# while r pointer < len(prices), we check for 2 things:
+#  - if price on day r > price on day l, we calculate profit and update currMax if profit > currMax and then increment r
+#  - if price on day r < price on day l, we shift l to r's spot because we found a new minima and we want to increment r 
+#      so we can keep finiding new maxima to maximize profit
+#
 # at the end, return maxProfit
 
-# Space Complexity: O(n)
 # Time  Complexity: O(n)
+# Space Complexity: O(1)
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        if len(prices) == 1:
-            return 0
-        currMin = prices[0]
-        maxProfit = 0
-        for i in range(1, len(prices)):
-            currProfit = (prices[i] - currMin)
-            if (currProfit > maxProfit):
-                maxProfit = currProfit
-            if (prices[i] < currMin):
-                currMin = prices[i]
-        return maxProfit
+        currMax = 0
+        l, r = 0, 1
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                currMax = max(profit, currMax)
+            else:
+                l = r
+            r += 1
+
+        return currMax
