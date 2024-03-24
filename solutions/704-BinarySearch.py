a3d9f363f2f2c1f2fa02ca0a51755821bf8445ed
Reyansh Patange <35512531+Reyansh14@users.noreply.github.com>
2024-03-24 18:01:43 -0400

    Solved LC #701: Binary Search

---
+++ solutions/704-BinarySearch.py
@@ -0,0 +1,17 @@
+# Notes: 
+# 
+# Time  Complexity: O(log(n))
+# Space Complexity: O(1)
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        low, high = 0, len(nums) - 1
+        while low <= high:
+            mid = int((low + high)/2)
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                low = mid + 1
+            elif nums[mid] > target:
+                high = mid - 1
+        return -1
